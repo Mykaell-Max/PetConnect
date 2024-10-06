49a8eccf6668e1b90b4a34bfe055967b0d973dc5
Breno Palma Miele Aniceto <brenomiele@ufu.br>
2024-10-05 23:34:32 -0300

    Features/Chats Routes

--- api/api.js
+++ api/api.js
@@ -3,10 +3,12 @@ const api = express();
 
 const userRoutes = require("./routes/userRoutes");
 const petRoutes = require("./routes/petRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 api.use(express.json());
 
 api.use('/api/users', userRoutes);
 api.use('/api/pets', petRoutes);
+api.use('/api/chats', chatRoutes);
 
 
 api.listen(8000, () => {console.log('Running')});
